5caefb7f1e4a471c8ffba9c4e1dc07b1728d3b6e
Topher <topherpsybun@gmail.com>
2026-05-24 15:22:53 +1000

    topher main scene

---
+++ Unity Project/Assets/Scripts/LevelSwitch.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+using UnityEngine;
+
+public class LevelSwitch : MonoBehaviour
+{
+    GameManager gameManager;
+    public string nextLevel;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        gameManager = GameObject.FindGameObjectWithTag("GameManager").GetComponent<GameManager>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider otherObject)
+    {
+        if (otherObject.transform.tag == "Player" && gameManager.levelComplete)
+        {
+            SceneManager.LoadScene(nextLevel);
+        }
+    }
+}
