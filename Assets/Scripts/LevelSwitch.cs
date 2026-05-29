71460a0668486191c50cf2dd1a682fabd3024c6c
MariusForonda <mariusforonda@gmail.com>
2026-05-29 15:00:17 +1000

    Marius Unity Project
    Scripts are included as well as probuilder and necessary UI elements

---
+++ Assets/Scripts/LevelSwitch.cs
@@ -0,0 +1,23 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelSwitch : MonoBehaviour
+{
+    GameManager gameManager;
+    public string nextLevel;
+
+    private void Start()
+    {
+        gameManager = GameObject.FindGameObjectWithTag("GameManager").GetComponent<GameManager>();
+    }
+    private void OnTriggerEnter(Collider otherObject)
+    {
+        if (otherObject.transform.tag == "Player")
+        {
+            if (gameManager.levelComplete)
+            {
+                SceneManager.LoadScene(nextLevel);
+            }
+        }
+    }
+}
